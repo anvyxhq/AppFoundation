6c859c4b10258db8f793113a1497117972a204f2
imx <phtuanh.imx@gmail.com>
2026-07-02 20:39:10 +0700

    chore!: raise minimum deployment to iOS 26; drop pre-26 availability guards

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AppFoundation",
     platforms: [
-        .iOS(.v17)
+        .iOS("26.0")
     ],
     products: [
         .library(name: "AppFoundation", targets: ["AppFoundation"]),
